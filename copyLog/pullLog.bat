5a8ee83c361ea8220066edfcc626eaa926065175
SamYangzx <chengzhang_fh@163.com>
2020-01-05 15:22:23 +0800

    modify pullLog

--- copyLog/pullLog.bat
+++ copyLog/pullLog.bat
@@ -1,11 +1,25 @@
+
 adb root
 adb remount
 
+@echo off
+set d=%date:~0,4%-%date:~5,2%-%date:~8,2%
+set hour=%time:~,2%
+set min_sec=%time:~3,2%-%time:~6,2%
+if "%time:~,1%"==" " set hour=0%time:~1,1%
+set t=%hour%-%min_sec%
+@echo on
+set FOLDER=%d%_%t%
 
-adb pull /storage/sdcard0/mtklog/mobilelog/ ./mobilelog/
-adb pull /data/anr/ ./anr/
-adb pull /data/tombstone/ .tombstone/
+mkdir %FOLDER%
 
-adb shell rm -r /data/anr/
-adb shell rm -r /storage/sdcard0/mtklog/mobilelog/
-adb shell rm -r /data/tombstone/
+adb pull /cache/recovery/ %FOLDER%/
+adb pull /data/anr/ %FOLDER%/
+adb pull /data/tombstones/ %FOLDER%/
+adb pull /sdcard/mtklog/ %FOLDER%/
+adb logcat -G 20m
+adb logcat > %FOLDER%/logcat.log
+
+
+
+
